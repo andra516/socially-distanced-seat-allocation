ec846a4596ddf0096c5930eaa1ba72a44ab6256d
Henry Andralojc <henry.andralojc@live.co.uk>
2021-07-12 18:08:50 +0100

    fully implemented JSON file saving for session data

--- seatAllocationIO.py
+++ seatAllocationIO.py
@@ -25,9 +25,9 @@ def savePlanDetails(saveDirectory, ppi, pdfScale, magicScale):
                                   "magicScale" : magicScale}
                     }
     
-    planDetailsPath = saveDirectory + os.sep + 'planDetails.json'
+    sessionSavePath = saveDirectory + os.sep + 'allocationSaveData.json'
     
-    with open(planDetailsPath, 'w') as f:
+    with open(sessionSavePath, 'w') as f:
         json.dump(planDetails, f)
         
     pass
@@ -38,10 +38,8 @@ def saveSession(saveDirectory, Xs, Ys):
     '''
     Function that saves the progress made in selecting seats in a given room plan.
     
-    - Saves a numpy array of seat coordinates (in pixels on the .png) to a .txt file named : 'Seat Allocation Save Data.txt'
-    - Eventually will also save indices of selected seats which have been chosen for the allocation to a seperate .txt file
-    
-    (or everything is saved to a .json file)
+    - Saves a numpy array of seat coordinates (in pixels on the .png) to the allocation session .json file
+    - Eventually will also save indices of selected seats which have been chosen for the allocation to the .json save file.
     
     Inputs:\n
         > directory, str of directory path that data should save to.\n
@@ -49,15 +47,27 @@ def saveSession(saveDirectory, Xs, Ys):
         > Ys - list of floats of all Y coords.\n
     
     '''     
-    ## SAVE SELECTED SEAT PIXEL COORDINATES
+    ## Read the JSON thats there already:
+    for file in os.listdir(saveDirectory):
+        if os.path.splitext(file)[1] == '.json':
+            saveSessionFile = file
+            break
+    
+    sessionSavePath = saveDirectory + os.sep + saveSessionFile
+    
+    with open(sessionSavePath, 'r') as f:
+        sessionSaveData = json.load(f)
+    
+    ## Cast selected seat positions to numpy array, then list
     seatPixelCoords = np.zeros(shape=(len(Xs),2))
     seatPixelCoords[:,0] = Xs
     seatPixelCoords[:,1] = Ys
-
-    seatCoordFilePath = saveDirectory + os.sep + 'Seat Allocation Save Data.txt'
-    np.savetxt(seatCoordFilePath, seatPixelCoords)
-
-    # (eventually) SAVE ALLOCATED SEAT COORDS/INDICES and replace with .json file
+    
+    sessionSaveData['seatPixelCoords'] = seatPixelCoords.tolist()
+    
+    with open(sessionSavePath, 'w') as f:
+        json.dump(sessionSaveData, f)
+        
 
 def loadSession(directory):
     '''
@@ -75,18 +85,13 @@ def loadSession(directory):
         if not:
         returns None indicating there was an error
     '''
-    ### CHECK EXACTLY ONE .PNG AND ONE .TXT FILE - png will be image of lecture hall
-    # txt will be seatPixelCoords
-    numTXTs = 0
+    ### CHECK FOLDER CONTAINS WHAT YOU EXPECT
     numPNGs = 0
     numPDFs = 0
     numJSONs = 0
     numElse = 0
     for file in os.listdir(directory):
-        if os.path.splitext(file)[1] == '.txt':
-            numTXTs += 1
-            sessionDataFile = file
-        elif os.path.splitext(file)[1] == '.png':
+        if os.path.splitext(file)[1] == '.png':
             numPNGs += 1
             PNGFile = file
         elif os.path.splitext(file)[1] == '.pdf':
@@ -94,24 +99,26 @@ def loadSession(directory):
             PDFFile = file
         elif os.path.splitext(file)[1] == '.json':
             numJSONs += 1
-            JSONFile = file
+            sessionSaveFile = file
         else:
             numElse += 1
-        
+    
             
-    if numTXTs != 1 or numPNGs != 1 or numPDFs != 1 or numJSONs != 1 or numElse != 0:
-        print('File should only contain exactly one .png, one .pdf, one .txt and one .json file')
+    if numPNGs != 1 or numPDFs != 1 or numJSONs != 1 or numElse != 0:
+        print('File should only contain exactly one .png, one .pdf and one .json file')
         return None
     
     ## find PNGFilePath
-    sessionSaveDataPath = directory + os.sep + sessionDataFile
+    sessionSaveDataPath = directory + os.sep + sessionSaveFile
     PNGFilePath = directory + os.sep + PNGFile
-    ## load the seat coords
-    seatPixelCoords = np.loadtxt(sessionSaveDataPath)
-    ## load the magicScale
-    with open((directory+os.sep+JSONFile), 'r') as f:
-        planDetails = json.load(f)['planDetails']
     
+    ## load the seat coords
+    with open(sessionSaveDataPath, 'r') as f:
+        sessionSaveData = json.load(f)
+    
+    seatPixelCoords = np.array(sessionSaveData['seatPixelCoords'])
+    ## load the magicScale
+    planDetails = sessionSaveData['planDetails']
     magicScale = planDetails['magicScale']
     
     return (PNGFilePath, seatPixelCoords, magicScale)
